ca8bf5f4db91380ab3d1dbefd8aa08b7c13e23bb
Orochi <orochi.keydream@gmail.com>
2016-05-25 20:30:45 +0600

    Удалены неиспользуемые директивы using

--- MovementForm/MovementEnterForm.cs
+++ MovementForm/MovementEnterForm.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using AMovement;
 using UMovement;
 using OMovement;
-using IMovement;
 
 namespace MovementForm
 {
